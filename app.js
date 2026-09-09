ae55d4c30a38124e25b3a5ad4f7bf2859a61b154
Nilesh <117421926+Snakeye97@users.noreply.github.com>
2026-09-09 17:35:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL='PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_URL='https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec';
 const $=id=>document.getElementById(id);
 const state={customer:null};
 function esc(s){return String(s??'').replace(/[&<>"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m]))}
